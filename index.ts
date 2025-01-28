b6ba323d8d2ff566e3a71f83708712d8370e7069
NashibJr <nashibjr@gmail.com>
2025-01-28 18:43:45 +0300

    --no-edit

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("hello world", 123456);
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const main = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL!);
+
+    app.use(express.json(), express.urlencoded({ extended: true }));
+
+    app.listen(9090, () => console.log("Connected to the server"));
+  } catch (error) {
+    console.log(error, ">>>");
+  }
+};
+
+main();
